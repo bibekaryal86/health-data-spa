fb5f26dc2adf85ef8954512fc3c5f6e4ca36f7fd
Bibek Aryal <bibekaryal86@gmail.com>
2022-01-19 15:20:56 -0700

    Added Types

---
+++ src/checkup_results/index.ts
@@ -0,0 +1,5 @@
+// types
+import { CheckupResultState, CheckupResultType, DefaultCheckupResult } from './types/checkup.result.data.types'
+
+export { DefaultCheckupResult }
+export type { CheckupResultState, CheckupResultType }
